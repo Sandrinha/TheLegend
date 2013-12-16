b98ec1452561286d653207cdf7fad5a0fa33e93f
Sandra Monteiro <1050057@isep.ipp.pt>
2013-12-16 15:05:37 +0000

    MVC

---
+++ TheLegend/TheLegend/Models/Mission.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Web;
+
+namespace TheLegend.Models
+{
+    public class Mission
+    {
+        [DatabaseGenerated(DatabaseGeneratedOption.None)]
+
+        public int MissionID { get; set; }
+        public string MissionName {get; set;}
+        public int TargetUserID { get; set; }
+        public int MissionStateID { get; set; }
+        public int UserID { get; set; }
+
+    }
+}
---
+++ TheLegend/TheLegend/Models/User.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace TheLegend.Models
+{
+    public class User
+    {
+        public int UserID { get; set; }
+        public string Name { get; set; }
+        public string Birth { get; set; }
+        public string Sex { get; set; }
+        public string Email { get; set; }
+        public string Password { get; set; }
+
+        public virtual ICollection <Gamers> Registo { get; set; }
+    }
+}
